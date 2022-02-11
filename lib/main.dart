e911cf946e59607b73ea9caf36565a314fb39f98
nkalpak <nikita.kalpakovski@outlook.com>
2022-02-11 14:20:58 +0100

    feat: implement preview card

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,109 +8,153 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      theme: ThemeData(fontFamily: 'Outfit'),
+      home: SafeArea(
+        child: Container(
+          padding: EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+          color: CustomColors.veryDarkBlueMain,
+          child: Card(
+            color: CustomColors.veryDarkBlueCard,
+            child: Padding(
+              padding: EdgeInsets.all(32),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ClipRRect(
+                    child: Image.asset('assets/images/image-equilibrium.jpg'),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    'Equilibrium #3429',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 26,
+                        fontWeight: FontWeight.w600),
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    'Our Equilibrium collection promotes balance and calm.',
+                    style: TextStyle(
+                        fontWeight: FontWeight.w300,
+                        color: CustomColors.softBlue,
+                        fontSize: 18),
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    children: [
+                      CurrencyLabel(
+                          label: 'ETH',
+                          value: 0.041,
+                          icon: SvgPicture.asset(
+                              'assets/images/icon-ethereum.svg')),
+                      Spacer(),
+                      IconLabel(
+                          icon: Icon(
+                            Icons.access_time_filled,
+                            color: CustomColors.softBlue,
+                          ),
+                          label: Text(
+                            '3 days left',
+                            style: TextStyle(
+                                fontSize: 18, color: CustomColors.softBlue),
+                          ))
+                    ],
+                  ),
+                  SizedBox(height: 8),
+                  Divider(
+                    color: CustomColors.softBlue,
+                  ),
+                  SizedBox(height: 8),
+                  IconLabel(
+                      icon: Container(
+                        child: CircleAvatar(
+                          foregroundImage:
+                          AssetImage('assets/images/image-avatar.png'),
+                        ),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(999),
+                          border: Border.all(color: Colors.white70)
+                        ),
+                      ),
+                      label: RichText(
+                        text: TextSpan(
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.w300),
+                            children: [
+                              TextSpan(
+                                  text: 'Creation of ',
+                                  style:
+                                      TextStyle(color: CustomColors.softBlue)),
+                              TextSpan(text: 'Jules Wyvern')
+                            ]),
+                      ))
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+/*
+* 1. How to propagate color to multiple (different) elements down the tree?
+* Imagine that we have a parent Wrapper and two children, an Icon and a Text.
+* How do we specify the color of *both* the Icon and the Text via the Wrapper,
+* and have them inherit the color?
+*
+*
+* */
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+class IconLabel extends StatelessWidget {
+  final Widget icon, label;
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  const IconLabel({required this.icon, required this.label});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    return Row(
+      children: [icon, SizedBox(width: 8), label],
     );
   }
+}
+
+class CurrencyLabel extends StatelessWidget {
+  final String label;
+  final double value;
+  final Widget icon;
+
+  const CurrencyLabel(
+      {Key? key, required this.label, required this.value, required this.icon})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return IconLabel(
+        icon: icon,
+        label: Text(
+          '$value ${label.toUpperCase()}',
+          style: TextStyle(
+              color: HSLColor.fromAHSL(1.0, 178, 1.0, 0.5).toColor(),
+              fontSize: 18,
+              fontWeight: FontWeight.w600),
+        ));
+  }
+}
+
+class CustomColors {
+  static Color cyan = const HSLColor.fromAHSL(1.0, 187, 1.0, 0.5).toColor();
+  static Color softBlue =
+      const HSLColor.fromAHSL(1.0, 215, 0.51, 0.7).toColor();
+  static Color veryDarkBlueMain =
+      const HSLColor.fromAHSL(1.0, 217, 0.54, 0.11).toColor();
+  static Color veryDarkBlueCard =
+      const HSLColor.fromAHSL(1.0, 216, 0.5, 0.16).toColor();
+  static Color veryDarkBlueLine =
+      const HSLColor.fromAHSL(1.0, 215, 0.32, 0.27).toColor();
 }
